6aa5ec9c78e2fa779d3d5b2bbfea66024ec26cfa
Vladislav Krestinin <vlad.krs7@gmail.com>
2020-04-05 13:02:22 +0300

    * Stats - staging
    * Statistics page: Done

--- src/JustSending/Controllers/HomeController.cs
+++ src/JustSending/Controllers/HomeController.cs
@@ -7,14 +7,42 @@ using Microsoft.AspNetCore.Mvc;
 using System.Linq;
 using IOFile = System.IO.File;
 using System;
+using JustSending.Data;
 
 namespace JustSending.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly AppDbContext _db;
+        public HomeController(AppDbContext db)
+        {
+            _db = db;
+        }
+
         public IActionResult Index()
         {
             return View();
+        }
+
+        [Route("stats")]
+        public IActionResult Stats(int date = -1)
+        {
+            var stat = _db.Statistics.FindById(date);
+            if(stat == null) return NotFound();
+
+            return View(stat);
+        }
+
+        [Route("api/prime")]
+        public IActionResult Prime([FromServices] IHostingEnvironment env)
+        {
+            Response.Headers.Add("Access-Control-Allow-Origin", "*");
+            
+            return Json(new
+            {
+                Size_1024 = Helper.GetPrime(1024, env),
+                Size_2 = Helper.GetPrime(2, env)
+            });
         }
 
         public IActionResult Error()
